8fd35fac0f3df1a2e9f7e926fb91de8e750a8758
Zeeshan Khan <zeeshank1@gmail.com>
2021-04-11 22:48:41 +0530

    FEAT : added code in app.js for todo functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
 import React, { Component } from "react";
 
-function App() {
-    return <div className="App">Hello World</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+
+  updateInput(key, value) {
+    //update react state
+    this.setState({ [key]: value });
+  }
+
+  addItem() {
+    //create a newItem with unique Id
+    const itemObj = {
+      id: 1 + Math.random(),
+      value: this.state.newItem,
+    };
+
+    //copy the list object
+    const listCopy = [...this.state.list];
+
+    //updating list object
+    listCopy.push(itemObj);
+
+    //update state
+    this.setState({
+      list: listCopy,
+      newItem: "",
+    });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter((item) => item.id !== id);
+    this.setState({
+      list: updatedList,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Add an item ...
+        <br />
+        <input
+          type="text"
+          value={this.state.item}
+          onChange={(e) => this.updateInput("newItem", e.target.value)}
+        />
+        <button onClick={() => this.addItem()}>Add</button>
+        <br />
+        <ul>
+          {this.state.list.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <button onClick={() => this.deleteItem(item.id)}>X</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
